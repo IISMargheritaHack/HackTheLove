b45afad834e3f4e31612cd5f46965f6dd7d57c89
pipvice <vicedomini.giuseppepio@gmail.com>
2025-01-20 19:16:00 +0100

    Authentication button

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="443648413060-db7g7i880qktvmlemmcnthg4qptclu2l.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
