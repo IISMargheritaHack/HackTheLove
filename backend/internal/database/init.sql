c948ed71d4750a96960628d4c929564696a9a111
akiidjk <dragonfaiver@gmail.com>
2025-01-21 21:36:25 +0100

    Fix primary key and foreign key references in database schema

--- backend/internal/database/init.sql
+++ backend/internal/database/init.sql
@@ -6,17 +6,16 @@ CREATE TABLE IF NOT EXISTS surveys_response (
 
 -- Table Users
 CREATE TABLE IF NOT EXISTS users (
-    id_user SERIAL PRIMARY KEY, -- Primary key
+    email SERIAL PRIMARY KEY, -- Primary key
     id_surveys INTEGER REFERENCES surveys_response(id_surveys) ON DELETE SET NULL, -- Id of the survey
     name VARCHAR(255) NOT NULL, -- Name
-    email VARCHAR(255) UNIQUE NOT NULL, -- Unique email
     date_created TIMESTAMP DEFAULT NOW() -- Date of creation
 );
 
 -- Table Images
 CREATE TABLE IF NOT EXISTS images  (
     id_image SERIAL PRIMARY KEY, -- Primary key
-    id_user INTEGER REFERENCES users(id_user) ON DELETE CASCADE, -- User reference
+    email_user INTEGER REFERENCES users(email) ON DELETE CASCADE, -- User reference
     lo_oid OID NOT NULL, -- File reference
     uploaded_at TIMESTAMP DEFAULT NOW(), -- Date of upload
     metadata JSONB -- Extra information
