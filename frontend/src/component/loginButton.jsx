1dcbd00fc159f9d8db52976322cbc3cf1fccb19e
pipvice <vicedomini.giuseppepio@gmail.com>
2025-01-21 18:05:20 +0100

    added login page

---
+++ frontend/src/component/loginButton.jsx
@@ -0,0 +1,23 @@
+import { GoogleLogin } from '@react-oauth/google';
+import * as jwtDecode from 'jwt-decode';
+
+function LButton(){
+    return(
+        <GoogleLogin 
+        hosted_domain='iismargheritahackbaronissi.edu.it'
+        shape='pill'
+        width={'350px'}
+        onSuccess={(CredentialResponse) => {
+          const CredentialResponseDecoded = jwtDecode.jwtDecode(
+            CredentialResponse.credential
+          );
+          console.log(CredentialResponseDecoded);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+    );
+}
+
+export default LButton;
