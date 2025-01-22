2ee909d4692439609c5c4ea8f78f08a805100f02
akiidjk <dragonfaiver@gmail.com>
2025-01-22 15:56:14 +0100

    Add additional fields to users table and create questions table in database schema

--- backend/internal/database/init.sql
+++ backend/internal/database/init.sql
@@ -9,6 +9,13 @@ CREATE TABLE IF NOT EXISTS users (
     email SERIAL PRIMARY KEY, -- Primary key
     id_surveys INTEGER REFERENCES surveys_response(id_surveys) ON DELETE SET NULL, -- Id of the survey
     name VARCHAR(255) NOT NULL, -- Name
+    cognome VARCHAR(255) NOT NULL, -- Surname
+    phone VARCHAR(10), -- Phone
+    sex BOOL, -- Sex
+    bio TEXT, -- Biography
+    age INTEGER, -- Age
+    section VARCHAR(1) -- Section
+
     date_created TIMESTAMP DEFAULT NOW() -- Date of creation
 );
 
@@ -19,4 +26,14 @@ CREATE TABLE IF NOT EXISTS images  (
     lo_oid OID NOT NULL, -- File reference
     uploaded_at TIMESTAMP DEFAULT NOW(), -- Date of upload
     metadata JSONB -- Extra information
+);
+
+-- Questions and answers table
+
+CREATE TABLE IF NOT EXISTS questions (
+    id_question SERIAL PRIMARY KEY, -- Primary key
+    question TEXT NOT NULL, -- Question
+    type VARCHAR(255) NOT NULL, -- Type of question
+    options JSONB -- Options for the question
+    possible_answers JSONB -- Possible answers
 );
