ca70fd025cf84a9cdac0b9dcc0960a98ebe9d7c6
akiidjk <dragonfaiver@gmail.com>
2025-01-25 18:23:10 +0100

    feat: Add email validation function for specific domain in utils

--- backend/internal/utils.go
+++ backend/internal/utils.go
@@ -1,10 +1,37 @@
 package internal
 
-import "os"
+import (
+	"net/mail"
+	"os"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var l = GetLogger()
 
 func GetEnv(key, defaultValue string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
 	return defaultValue
+}
+
+func EmailHost(fl validator.FieldLevel) bool {
+	email := fl.Field().String()
+	parsedEmail, err := mail.ParseAddress(email)
+	if err != nil {
+		return false
+	}
+
+	parts := strings.Split(parsedEmail.Address, "@")
+	if len(parts) != 2 {
+		return false
+	}
+
+	if parts[1] == "iismargheritahackbaronissi.edu.it" {
+		return true
+	}
+
+	return false
 }
