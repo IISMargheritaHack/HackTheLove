6709fea55383815de16852bdd1e7be60954597bf
akiidjk <dragonfaiver@gmail.com>
2025-01-25 18:36:56 +0100

    fix: Update users table schema to use VARCHAR for email and surname fields

--- backend/internal/database/init.sql
+++ backend/internal/database/init.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS surveys_response (
 
 -- Table Users
 CREATE TABLE IF NOT EXISTS users (
-    email SERIAL PRIMARY KEY, -- Primary key
+    email VARCHAR(100) PRIMARY KEY, -- Primary key
     id_surveys INTEGER REFERENCES surveys_response(id_surveys) ON DELETE SET NULL, -- Id of the survey
     name VARCHAR(255) NOT NULL, -- Name
-    cognome VARCHAR(255) NOT NULL, -- Surname
+    surname VARCHAR(255) NOT NULL, -- Surname
     phone VARCHAR(10), -- Phone
     sex BOOLEAN, -- Sex
     bio TEXT, -- Biography
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS users (
 -- Table Images
 CREATE TABLE IF NOT EXISTS images (
     id_image SERIAL PRIMARY KEY, -- Primary key
-    email_user INTEGER REFERENCES users(email) ON DELETE CASCADE, -- User reference
+    email_user VARCHAR(100) REFERENCES users(email) ON DELETE CASCADE, -- User reference
     lo_oid OID NOT NULL, -- File reference
     uploaded_at TIMESTAMP DEFAULT NOW(), -- Date of upload
     metadata JSONB -- Extra information
