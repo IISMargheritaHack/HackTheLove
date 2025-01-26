50ea71dfcd3cd52f519cd038659a9e358329d814
akiidjk <dragonfaiver@gmail.com>
2025-01-26 11:44:23 +0100

    feat: Implement routing for LoginPage and IntroPage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,11 @@ import './App.css';
 import React from 'react';
 import LoginPage from '@pages/loginPage/loginPage';
 import { isMobile } from 'react-device-detect';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import IntroPage from '@pages/introPage/introPage.jsx';
 
 function App() {
+
   const AlertMessageDesktop =
     'ATTENTO Il sito è disponibile solo da cellulare!';
 
@@ -15,7 +18,15 @@ function App() {
       }
     }
   }, []);
-  return <LoginPage></LoginPage>;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> {/* Homepage */}
+        <Route path="/introPage" element={<IntroPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import IntroPage from '@pages/introPage/introPage.jsx';
 
 const devTokenOAuth =
   '443648413060-db7g7i880qktvmlemmcnthg4qptclu2l.apps.googleusercontent.com';
@@ -12,12 +10,7 @@ const devTokenOAuth =
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={devTokenOAuth}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} /> {/* Homepage */}
-          <Route path="/introPage" element={<IntroPage />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </GoogleOAuthProvider>
   </StrictMode>
 );
