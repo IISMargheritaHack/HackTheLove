427fc0d43a865f641e3ff525577ee1b80590fba3
akiidjk <dragonfaiver@gmail.com>
2025-01-29 22:51:50 +0100

    Add configuration management for application settings and database credentials

---
+++ backend/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"backend/internal/utils"
+	_ "embed"
+)
+
+// App
+var LogLevel = utils.GetEnv("LOG_LEVEL", "debug")
+var LogFile = utils.GetEnv("LOG_FILE", "")
+
+// Security configuration
+var JwtSecret = []byte(utils.GetEnv("SECRET_KEY", "secretkey"))
+var GoogleClientID = utils.GetEnv("GOOGLE_CLIENT_ID", "443648413060-db7g7i880qktvmlemmcnthg4qptclu2l.apps.googleusercontent.com")
+
+// Server
+var AppPort = utils.GetEnv("APP_PORT", "8080")
+var GINMODE = utils.GetEnv("GIN_MODE", "debug")
+
+// Database
+var DatabaseName = utils.GetEnv("DB_DATABASE", "postgres")
+var PasswordDatabase = utils.GetEnv("DB_PASSWORD", "password")
+var UsernameDatabase = utils.GetEnv("DB_USERNAME", "username")
+var PortDatabase = utils.GetEnv("DB_PORT", "5432")
+var HostDatabase = utils.GetEnv("DB_HOST", "localhost")
+var SchemaDatabase = utils.GetEnv("DB_SCHEMA", "public")
+
+// Files embedded
+
+//go:embed questions.json
+var Questions []byte
+
+//go:embed init.sql
+var TablesQueries string
