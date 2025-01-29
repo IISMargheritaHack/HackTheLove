5aa5ab84cc92b28abf845bd6222b836cfa77fb16
akiidjk <dragonfaiver@gmail.com>
2025-01-29 22:52:59 +0100

    Refactor imports

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
-	"backend/internal"
+	"backend/config"
+	"backend/internal/logger"
+	"backend/internal/models"
 	"context"
 	"database/sql"
 	_ "embed"
@@ -13,9 +15,6 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
 )
-
-//go:embed init.sql
-var tablesQueries string
 
 // Service represents a service that interacts with a database.
 type Service interface {
@@ -33,16 +32,10 @@ type service struct {
 }
 
 var (
-	database   = internal.GetEnv("DB_DATABASE", "postgres")
-	password   = internal.GetEnv("DB_PASSWORD", "password")
-	username   = internal.GetEnv("DB_USERNAME", "username")
-	port       = internal.GetEnv("DB_PORT", "5432")
-	host       = internal.GetEnv("DB_HOST", "localhost")
-	schema     = internal.GetEnv("DB_SCHEMA", "public")
 	dbInstance *service
 )
 
-var l = internal.GetLogger()
+var log = logger.GetLogger()
 
 // For user the instance: db.(*service).db
 func New() Service {
@@ -50,13 +43,13 @@ func New() Service {
 	if dbInstance != nil {
 		return dbInstance
 	}
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", username, password, host, port, database, schema)
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", config.UsernameDatabase, config.PasswordDatabase, config.HostDatabase, config.PortDatabase, config.DatabaseName, config.SchemaDatabase)
 
-	l.Debug().Msg(connStr)
+	log.Debug().Msg(connStr)
 
 	db, err := sql.Open("pgx", connStr)
 	if err != nil {
-		l.Fatal().Err(err).Msg("Failed to open database connection")
+		log.Fatal().Err(err).Msg("Failed to open database connection")
 	}
 	dbInstance = &service{
 		db: db,
@@ -64,12 +57,25 @@ func New() Service {
 	return dbInstance
 }
 
+// ----- UTILS -----
+
 func InitTable(db Service) {
 	dbInstance := db.(*service).db
-	_, err := dbInstance.Exec(tablesQueries)
+	_, err := dbInstance.Exec(config.TablesQueries)
 	if err != nil {
-		l.Fatal().Err(err).Msg("Failed to initialize tables")
+		log.Fatal().Err(err).Msg("Failed to initialize tables")
 	}
+}
+
+func CheckIfUserExists(email string) bool {
+	user, err := GetUser(email)
+	if err != nil {
+		return false
+	}
+	if user.User.Email == "" {
+		return false
+	}
+	return true
 }
 
 // Health checks the health of the database connection by pinging the database.
@@ -85,7 +91,7 @@ func (s *service) Health() map[string]string {
 	if err != nil {
 		stats["status"] = "down"
 		stats["error"] = fmt.Sprintf("db down: %v", err)
-		l.Error().Err(err).Msg("Database is down")
+		log.Error().Err(err).Msg("Database is down")
 		return stats
 	}
 
@@ -106,22 +112,22 @@ func (s *service) Health() map[string]string {
 	// Evaluate stats to provide a health message
 	if dbStats.OpenConnections > 40 { // Assuming 50 is the max for this example
 		stats["message"] = "The database is experiencing heavy load."
-		l.Warn().Str("message", stats["message"]).Msg("Database load warning")
+		log.Warn().Str("message", stats["message"]).Msg("Database load warning")
 	}
 
 	if dbStats.WaitCount > 1000 {
 		stats["message"] = "The database has a high number of wait events, indicating potential bottlenecks."
-		l.Warn().Str("message", stats["message"]).Msg("Database wait event warning")
+		log.Warn().Str("message", stats["message"]).Msg("Database wait event warning")
 	}
 
 	if dbStats.MaxIdleClosed > int64(dbStats.OpenConnections)/2 {
 		stats["message"] = "Many idle connections are being closed, consider revising the connection pool settings."
-		l.Warn().Str("message", stats["message"]).Msg("Idle connections warning")
+		log.Warn().Str("message", stats["message"]).Msg("Idle connections warning")
 	}
 
 	if dbStats.MaxLifetimeClosed > int64(dbStats.OpenConnections)/2 {
 		stats["message"] = "Many connections are being closed due to max lifetime, consider increasing max lifetime or revising the connection usage pattern."
-		l.Warn().Str("message", stats["message"]).Msg("Max lifetime closed warning")
+		log.Warn().Str("message", stats["message"]).Msg("Max lifetime closed warning")
 	}
 
 	return stats
@@ -132,12 +138,14 @@ func (s *service) Health() map[string]string {
 // If the connection is successfully closed, it returns nil.
 // If an error occurs while closing the connection, it returns the error.
 func (s *service) Close() error {
-	l.Info().Msgf("Disconnected from database: %s", database)
+	log.Info().Msgf("Disconnected from database: %s", config.DatabaseName)
 	return s.db.Close()
 }
 
-func GetUser(email string) (internal.CompleteUser, error) {
-	var user internal.CompleteUser
+// ------- GET -------
+
+func GetUser(email string) (models.CompleteUser, error) {
+	var user models.CompleteUser
 
 	query := `
 		SELECT email, name, surname, phone, bio, age, sex, section
@@ -157,19 +165,19 @@ func GetUser(email string) (internal.CompleteUser, error) {
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return internal.CompleteUser{}, fmt.Errorf("no user found with email: %s", email)
+			return models.CompleteUser{}, fmt.Errorf("no user found with email: %s", email)
 		}
-		return internal.CompleteUser{}, fmt.Errorf("error querying user: %w", err)
+		return models.CompleteUser{}, fmt.Errorf("error querying user: %w", err)
 	}
 
 	return user, nil
 }
 
-func GetSurvey(email string) (internal.Survey, error) {
-	var survey internal.Survey
+func GetSurvey(email string) (models.Survey, error) {
+	var survey models.Survey
 
 	if !CheckIfUserExists(email) {
-		return internal.Survey{}, fmt.Errorf("user with email %s already exists", email)
+		return models.Survey{}, fmt.Errorf("user with email %s already exists", email)
 	}
 
 	query := `
@@ -182,15 +190,106 @@ func GetSurvey(email string) (internal.Survey, error) {
 	err := dbInstance.db.QueryRow(query, email).Scan(&survey.IdSurvey, &survey.Response)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return internal.Survey{}, fmt.Errorf("no survey found for email: %s", email)
+			return models.Survey{}, fmt.Errorf("no survey found for email: %s", email)
 		}
-		return internal.Survey{}, fmt.Errorf("error querying survey: %w", err)
+		return models.Survey{}, fmt.Errorf("error querying survey: %w", err)
 	}
 
 	return survey, nil
 }
 
-func AddUser(user internal.User) error {
+func GetImageIDByEmail(email string) (int, error) {
+	var imageID int
+
+	// Query per ottenere l'imageID
+	query := `
+		SELECT id_image
+		FROM images
+		WHERE email_user = $1
+		ORDER BY uploaded_at DESC
+		LIMIT 1;
+	`
+
+	err := dbInstance.db.QueryRow(query, email).Scan(&imageID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, fmt.Errorf("no images found for email: %s", email)
+		}
+		return 0, fmt.Errorf("failed to retrieve image ID: %w", err)
+	}
+
+	return imageID, nil
+}
+
+func GetPhoto(email string) ([]byte, error) {
+
+	imageID, err := GetImageIDByEmail(email)
+
+	log.Debug().Int("ImageID", imageID).Msg("ImageID received")
+
+	tx, err := dbInstance.db.Begin()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to start transaction: %w", err)
+	}
+	defer func() ([]byte, error) {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			commitErr := tx.Commit()
+			if commitErr != nil {
+				return nil, fmt.Errorf("failed to commit transaction: %w", commitErr)
+			}
+		}
+		return nil, nil
+	}()
+
+	var loOID uint32
+	err = tx.QueryRow("SELECT lo_oid FROM images WHERE id_image = $1", imageID).Scan(&loOID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("image with ID %d not found", imageID)
+		}
+		return nil, fmt.Errorf("failed to retrieve lo_oid: %w", err)
+	}
+
+	log.Debug().Uint32("loOID", loOID).Msg("loOID received")
+
+	var loFd int
+	err = tx.QueryRow("SELECT lo_open($1, $2)", loOID, 0x40000).Scan(&loFd) // 0x40000 = modalità lettura
+	if err != nil {
+		return nil, fmt.Errorf("failed to open large object: %w", err)
+	}
+
+	var fileData []byte
+	for {
+		var chunk []byte
+		err = tx.QueryRow("SELECT loread($1, $2)", loFd, 4096).Scan(&chunk)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				break
+			}
+			return nil, fmt.Errorf("failed to read large object: %w", err)
+		}
+		if len(chunk) == 0 {
+			break
+		}
+		fileData = append(fileData, chunk...)
+	}
+
+	defer func() {
+		_, closeErr := tx.Exec("SELECT lo_close($1)", loFd)
+		if closeErr != nil {
+			log.Error().Err(closeErr).Msg("failed to close large object")
+		}
+	}()
+
+	return fileData, nil
+}
+
+// --------- ADD ---------
+
+func AddUser(user models.User) error {
 	query := "INSERT INTO users (email, name, surname) VALUES ($1, $2, $3)"
 	result, err := dbInstance.db.Exec(query, user.Email, user.GivenName, user.FamilyName)
 	if err != nil {
@@ -206,12 +305,12 @@ func AddUser(user internal.User) error {
 		return fmt.Errorf("no rows were inserted for user %s", user.Email)
 	}
 
-	l.Debug().Msgf("User added successfully: %s (Rows affected: %d)", user.Email, rowsAffected)
+	log.Debug().Msgf("User added successfully: %s (Rows affected: %d)", user.Email, rowsAffected)
 
 	return nil
 }
 
-func AddUserInfo(user internal.UserInfo, email string) error {
+func AddUserInfo(user models.UserInfo, email string) error {
 
 	if !CheckIfUserExists(email) {
 		return fmt.Errorf("user with email %s already exists", email)
@@ -236,14 +335,14 @@ func AddUserInfo(user internal.UserInfo, email string) error {
 		return fmt.Errorf("no user found with email %s, no update applied", email)
 	}
 
-	l.Debug().Msgf("User info updated successfully for email %s (Rows affected: %d)", email, rowsAffected)
+	log.Debug().Msgf("User info updated successfully for email %s (Rows affected: %d)", email, rowsAffected)
 
 	return nil
 }
 
-func AddSurvey(survey internal.Survey, email string) error {
-	l.Debug().Str("Email", email).Msg("Email received")
-	l.Debug().Str("Response", survey.Response).Msg("Response received")
+func AddSurvey(survey models.Survey, email string) error {
+	log.Debug().Str("Email", email).Msg("Email received")
+	log.Debug().Str("Response", survey.Response).Msg("Response received")
 
 	if !CheckIfUserExists(email) {
 		return fmt.Errorf("user with email %s already exists", email)
@@ -270,16 +369,16 @@ func AddSurvey(survey internal.Survey, email string) error {
 	queryInsert := "INSERT INTO surveys (response) VALUES ($1) RETURNING id_survey"
 	err = tx.QueryRow(queryInsert, survey.Response).Scan(&surveyID)
 	if err != nil {
-		l.Error().Err(err).Str("Response", survey.Response).Msg("Failed to insert survey")
+		log.Error().Err(err).Str("Response", survey.Response).Msg("Failed to insert survey")
 		return fmt.Errorf("error inserting survey and retrieving ID: %w", err)
 	}
 
-	l.Debug().Str("SurveyID", surveyID).Msg("Survey ID generated")
+	log.Debug().Str("SurveyID", surveyID).Msg("Survey ID generated")
 
 	queryUpdate := "UPDATE users SET id_survey = $1 WHERE email = $2"
 	result, err := tx.Exec(queryUpdate, surveyID, email)
 	if err != nil {
-		l.Error().Err(err).Str("SurveyID", surveyID).Str("Email", email).Msg("Failed to update user with survey ID")
+		log.Error().Err(err).Str("SurveyID", surveyID).Str("Email", email).Msg("Failed to update user with survey ID")
 		return fmt.Errorf("error updating user with survey ID: %w", err)
 	}
 
@@ -289,18 +388,18 @@ func AddSurvey(survey internal.Survey, email string) error {
 	}
 
 	if rowsAffected == 0 {
-		l.Warn().Str("Email", email).Msg("No user updated with survey ID")
+		log.Warn().Str("Email", email).Msg("No user updated with survey ID")
 		return fmt.Errorf("no user found with email %s", email)
 	}
 
-	l.Debug().Str("Email", email).Str("SurveyID", surveyID).Msg("Survey successfully linked to user")
+	log.Debug().Str("Email", email).Str("SurveyID", surveyID).Msg("Survey successfully linked to user")
 
 	return nil
 }
 
 func AddPhoto(email string, imageFile io.Reader) error {
 
-	l.Debug().Str("Email", email).Msg("Email received")
+	log.Debug().Str("Email", email).Msg("Email received")
 
 	tx, err := dbInstance.db.Begin()
 	if err != nil {
@@ -360,104 +459,4 @@ func AddPhoto(email string, imageFile io.Reader) error {
 	}
 
 	return nil
-}
-
-func GetImageIDByEmail(email string) (int, error) {
-	var imageID int
-
-	// Query per ottenere l'imageID
-	query := `
-		SELECT id_image
-		FROM images
-		WHERE email_user = $1
-		ORDER BY uploaded_at DESC
-		LIMIT 1;
-	`
-
-	err := dbInstance.db.QueryRow(query, email).Scan(&imageID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return 0, fmt.Errorf("no images found for email: %s", email)
-		}
-		return 0, fmt.Errorf("failed to retrieve image ID: %w", err)
-	}
-
-	return imageID, nil
-}
-
-func GetPhoto(email string) ([]byte, error) {
-
-	imageID, err := GetImageIDByEmail(email)
-
-	l.Debug().Int("ImageID", imageID).Msg("ImageID received")
-
-	tx, err := dbInstance.db.Begin()
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to start transaction: %w", err)
-	}
-	defer func() ([]byte, error) {
-		if err != nil {
-			tx.Rollback()
-		} else {
-			commitErr := tx.Commit()
-			if commitErr != nil {
-				return nil, fmt.Errorf("failed to commit transaction: %w", commitErr)
-			}
-		}
-		return nil, nil
-	}()
-
-	var loOID uint32
-	err = tx.QueryRow("SELECT lo_oid FROM images WHERE id_image = $1", imageID).Scan(&loOID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("image with ID %d not found", imageID)
-		}
-		return nil, fmt.Errorf("failed to retrieve lo_oid: %w", err)
-	}
-
-	l.Debug().Uint32("loOID", loOID).Msg("loOID received")
-
-	var loFd int
-	err = tx.QueryRow("SELECT lo_open($1, $2)", loOID, 0x40000).Scan(&loFd) // 0x40000 = modalità lettura
-	if err != nil {
-		return nil, fmt.Errorf("failed to open large object: %w", err)
-	}
-
-	var fileData []byte
-	for {
-		var chunk []byte
-		err = tx.QueryRow("SELECT loread($1, $2)", loFd, 4096).Scan(&chunk)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				break
-			}
-			return nil, fmt.Errorf("failed to read large object: %w", err)
-		}
-		if len(chunk) == 0 {
-			break
-		}
-		fileData = append(fileData, chunk...)
-	}
-
-	defer func() {
-		_, closeErr := tx.Exec("SELECT lo_close($1)", loFd)
-		if closeErr != nil {
-			l.Error().Err(closeErr).Msg("failed to close large object")
-		}
-	}()
-
-	return fileData, nil
-}
-
-func CheckIfUserExists(email string) bool {
-	user, err := GetUser(email)
-	if err != nil {
-		return false
-	}
-	if user.User.Email == "" {
-		return false
-	}
-	return true
 }
