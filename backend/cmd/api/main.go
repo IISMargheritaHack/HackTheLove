2fb0b7e7ac0012f2f404676827760a48d0d0ed5e
akiidjk <dragonfaiver@gmail.com>
2025-01-29 22:51:42 +0100

    Refactor logging and graceful shutdown in API server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,63 +2,54 @@ package main
 
 import (
 	"context"
-	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"backend/internal"
+	"backend/config"
+	"backend/internal/logger"
 	"backend/internal/server"
 
 	"github.com/rs/zerolog"
 )
 
-var l = internal.GetLogger()
-
-func gracefulShutdown(apiServer *http.Server, done chan bool) {
-	// Create context that listens for the interrupt signal from the OS.
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	// Listen for the interrupt signal.
-	<-ctx.Done()
-
-	l.Info().Msg("Shutting down gracefully, press Ctrl+C again to force")
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := apiServer.Shutdown(ctx); err != nil {
-		l.Error().Err(err).Msg("Server forced to shutdown with error")
-	}
-
-	l.Info().Msg("Server exiting")
-
-	// Notify the main goroutine that the shutdown is complete
-	done <- true
-}
+var log = logger.GetLogger()
 
 func init() {
-	internal.SetLevel(zerolog.DebugLevel)
+	logLevel, err := zerolog.ParseLevel(config.LogLevel)
+	if err != nil {
+		logLevel = zerolog.InfoLevel
+	}
+	logFile := config.LogFile
+
+	logger.InitLogger(logger.Config{
+		Level:   logLevel,
+		LogFile: logFile,
+	})
 }
 
 func main() {
-
 	server := server.NewServer()
 
-	// Create a done channel to signal when the shutdown is complete
-	done := make(chan bool, 1)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
-	// Run graceful shutdown in a separate goroutine
-	go gracefulShutdown(server, done)
+	go func() {
+		if err := server.Run(); err != nil {
+			log.Fatal().Err(err).Msg("HTTP server error")
+		}
+	}()
 
-	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		l.Fatal().Err(err).Msg("HTTP server error")
+	<-ctx.Done()
+
+	log.Info().Msg("Shutting down gracefully, press Ctrl+C again to force")
+
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctxTimeout); err != nil {
+		log.Error().Err(err).Msg("Server forced to shutdown with error")
 	}
 
-	// Wait for the graceful shutdown to complete
-	<-done
-	l.Info().Msg("Graceful shutdown complete.")
+	log.Info().Msg("Server exiting")
 }
