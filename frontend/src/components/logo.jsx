b7ac6415f79150f20c9e9b3c6c589d17436b9532
akiidjk <dragonfaiver@gmail.com>
2025-02-02 16:08:12 +0100

    Add Logo component for displaying the application logo

---
+++ frontend/src/components/logo.jsx
@@ -0,0 +1,11 @@
+
+
+function Logo() {
+  return (
+    <div id="center" className="flex justify-center items-center">
+      <img className="max-w-[15vw] max-h-screen object-contain mx-auto block" src="logo.svg" alt="Logo" />
+    </div>
+  );
+}
+
+export default Logo;
