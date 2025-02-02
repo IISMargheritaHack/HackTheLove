25ec847e0b244df003efc807a66aec0aa570b3c5
akiidjk <dragonfaiver@gmail.com>
2025-02-02 19:04:10 +0100

    Refactor card components and styles for improved swipe functionality

--- frontend/src/pages/homePage/homePage.jsx
+++ frontend/src/pages/homePage/homePage.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import img1 from '@assets/img/gabibbo.webp';
 import img2 from '@assets/img/Martin_Luther_King_Jr.webp';
 import img3 from '@assets/img/pietro.webp';
 import img4 from '@assets/img/turetta.webp';
-import { Card, CardWrapper } from 'react-swipeable-cards';
+import Card from '@components/card';
+import CardWrapper from '@components/cardWrapper';
 
 const cardsData = [
   { id: '1', src: img1 },
@@ -27,27 +27,7 @@ function HomePage() {
           onSwipeLeft={() => onSwipeLeft(card)}
           onSwipeRight={() => onSwipeRight(card)}
           onDoubleTap={() => onDoubleTap(card)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#fff', // Imposta il colore di sfondo per evitare trasparenze
-            borderRadius: '12px',
-            overflow: 'hidden', // Evita che l'immagine si distorca
-            width: '320px', // Imposta una dimensione fissa
-            height: '480px',
-            boxShadow: '0px 4px 10px rgba(0,0,0,0.2)', // Aggiunge un'ombra per un look migliore
-          }}
         >
-          <img
-            src={card.src}
-            alt="Swipe Card"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover', // Assicura che l'immagine riempia la card senza muoversi
-            }}
-          />
         </Card>
       ))}
     </CardWrapper>
