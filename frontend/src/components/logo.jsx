085eeb0d2a2b3f285a56c838b1ba1cd483064141
akiidjk <dragonfaiver@gmail.com>
2025-02-03 16:21:57 +0100

    Adjust logo size and improve layout spacing on login page for better visual balance

--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -3,7 +3,7 @@
 function Logo() {
   return (
     <div id="center" className="flex justify-center items-center">
-      <img className="max-w-[15vw] max-h-screen object-contain mx-auto block" src="logo.svg" alt="Logo" />
+      <img className="max-w-[25vw] max-h-screen object-contain mx-auto block" src="logo.svg" alt="Logo" />
     </div>
   );
 }
