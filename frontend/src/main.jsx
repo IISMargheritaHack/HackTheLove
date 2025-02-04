0d37b98dea78725b29ad7bd2273ee7f58948cb6f
akiidjk <dragonfaiver@gmail.com>
2025-02-05 00:24:11 +0100

    Wrap GoogleOAuthProvider with UserProvider for user context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import UserProvider from '@provider/userProvider';
 
 const devTokenOAuth =
   '443648413060-db7g7i880qktvmlemmcnthg4qptclu2l.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={devTokenOAuth}>
-      <App />
-    </GoogleOAuthProvider>
+    <UserProvider>
+      <GoogleOAuthProvider clientId={devTokenOAuth}>
+        <App />
+      </GoogleOAuthProvider>
+    </UserProvider>
   </StrictMode>
 );
