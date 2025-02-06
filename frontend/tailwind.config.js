83891a5025ca53e5e0c5db96e4534417d849455d
akiidjk <dragonfaiver@gmail.com>
2025-02-06 23:35:05 +0100

    Update Tailwind config: include modal and spacer components in content path

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(button|dropdown|ripple|spinner|menu|divider|popover).js"
+    "./node_modules/@heroui/theme/dist/components/(button|dropdown|modal|spacer|ripple|spinner|menu|divider|popover).js"
 ],
   theme: {
     extend: {},
