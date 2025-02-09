1b8ed5553bfd75e612258e1e3c0bc9c0681e4a1f
akiidjk <dragonfaiver@gmail.com>
2025-02-09 15:04:00 +0100

    Refactor Vite configuration to improve build settings and resolve aliases using path module

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   allowedHosts: true,
-  // },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    "alias": {
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@icons': '/src/assets/icons',
-      '@images': '/src/assets/images',
-      '@pages': '/src/pages',
-      '@fonts': '/src/assets/fonts',
-      '@api': '/src/api/',
-      '@provider': '/src/provider',
-      '@config': '/src/config.js',
-      '@utils': '/src/utils',
-    }
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
-
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+      '@api': path.resolve(__dirname, 'src/api/'),
+      '@provider': path.resolve(__dirname, 'src/provider'),
+      '@config': path.resolve(__dirname, 'src/config.js'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+  define: {
+    'import.meta.env.MODE': '"production"',
+    'import.meta.env.PROD': 'true',
+  },
 })
